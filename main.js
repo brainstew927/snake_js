345a749f23235077d74dcbba0191944ed59a990f
lorenzo <brainstew927@gmail.com>
2019-04-18 23:43:21 +0200

    aggiornta

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //posizione iniziale del blocco
-var x = 100; 
-var y = 100;
+var x = 0; 
+var y = 0;
 //array posizioni
 var array_x = [];
 var array_y = [];
@@ -8,9 +8,6 @@ var array_y = [];
 array_x.push(x);
 array_y.push(y);
 
-//variabili per il movimento(se y_su = true il blocco va su, se x_de = true il blocco va a destra)
-y_su = false;
-x_de = true;
 //canvas è l'oggetto canvas che viene preso dall'html e cont è il contesto 
 var canv;
 var cont;
@@ -18,7 +15,13 @@ var cont;
 larghezza = 60
 altezza = 60;
 
-var lunghezza_snake;
+var lunghezza_snake = 2;
+
+
+var html_altez
+var html_lungh
+//direzione del movimento
+var direzione = -1
 
 class blocco{
     constructor(height, width){
@@ -27,45 +30,70 @@ class blocco{
     }
 };
 
+function main_handler(){
+    draw();
+}
+
 function start(){
     console.log("started!");
+
     canv = document.getElementById("canv")
     cont = canv.getContext("2d")
     ogg = new blocco(altezza, larghezza);
+
+    html_altez = document.getElementById("html_lh")
+    html_lungh = document.getElementById("html_wh")    
 }
 
-function move(){
+function key_handler(event){
+    console.log("pressed: key: " + event.key+ "")
+    if(event.key == "a"){
+        direzione = 3;
+    }
+    if(event.key == "s"){
+        direzione = 2;
+    }
+    if(event.key == "d"){
+        direzione = 1
+    }
+    if(event.key == "w"){
+        direzione = 0;
+    }
+    console.log("direzione attuale: " + direzione)
+}
+
+function move(){   
     
-    if(array_y[0] >= canv.height - ogg.height){
-        y_su = true;
+    switch (direzione) {
+        case 0:
+                if(array_y[0]-ogg.height >= 0){
+                    array_y[0] -= ogg.height;
+                }
+            break            
+        case 1:
+            if(array_x[0]+ogg.width <= canv.width-ogg.width){
+                array_x[0] += ogg.height;
+            }          
+            break
+        case 2:
+            if(array_y[0]+ogg.height <= canv.height-ogg.width){
+                array_y[0] += ogg.height;
+            }    
+            break
+        case 3:
+            if(array_x[0]-ogg.width >= 0){
+                array_x[0] -= ogg.height;
+            }               
+            break;
+        default:
+            console.log("errore mistico nel movimento")
+            break;
     }
-    if(array_y[0] <= 0){
-        y_su = false;
-    }   
 
-    if(array_x[0] >= canv.width - ogg.width){
-        x_de = false;
-    }
-    if(array_x[0] <= 0){
-        x_de = true;
-    }
 
-    if(x_de){
-        array_x[0]++;
-    }
-    if(!x_de){
-        array_x[0]--;
-    }
-    if(y_su){
-        array_y[0]--;
-    }
-    if(!y_su){
-        array_y[0]++;
-    }  
 }
-
+//funzione per 
 function clear_rect(){
-    console.log("chiamato "+ "\n" + "wg:"+ canv.width + "   hg:"+canv.height)
     cont.clearRect(0, 0, canv.width, canv.height);
 }
 //funzione che disegna il blocco 
@@ -74,11 +102,13 @@ function draw(){
     cont.beginPath();    
 
     cont.rect(array_x[0],array_y[0], ogg.width,ogg.width)
-   // console.log("width:" + canv.width + "  height:"+ canv.height)
+  
     cont.stroke();
     
     move()
-    
+    html_altez.innerHTML = array_y[0]
+    html_lungh.innerHTML = array_x[0]
     console.log("x:" + array_x[0]+ " y:" + array_y[0]);
 }
-setInterval(draw, 1);
+setInterval(main_handler, 80);
+
