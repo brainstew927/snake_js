58946ffa3831121422d59b3af989fb008e3d45cf
lorenzo <brainstew927@gmail.com>
2019-04-19 14:35:47 +0200

    lista aggiunte: -aggiunta possibilità per il serpente di allungarsi -aggiunto il colore ed i bordi per il serpente

--- main.js
+++ main.js
@@ -17,11 +17,14 @@ altezza = 60;
 
 var lunghezza_snake = 2;
 
-
+var html_lungh_snake
 var html_altez
 var html_lungh
 //direzione del movimento
 var direzione = -1
+//counter che conta tutte le volte che viene eseguito main_handler: se raggiunge un certo valore(counter_limit) aumenta di uno la lunghezza del serpente
+var counter_all = 0;
+var counter_limit = 30
 
 class blocco{
     constructor(height, width){
@@ -30,21 +33,39 @@ class blocco{
     }
 };
 
+
+
 function main_handler(){
+    counter_all++;
+
+    if(counter_all >= counter_limit){
+        add()
+        counter_all = 0;
+    }
+    
+    html_lungh_snake.innerHTML = array_x.length
     draw();
+
 }
 
 function start(){
     console.log("started!");
-
+    //prende il controllo dell'elemento canvas e dell'elemeno per disegnare sul canvas
     canv = document.getElementById("canv")
     cont = canv.getContext("2d")
+    
+  
+    //crea un nuovo oggetto per gestire i blocchi del serpente
     ogg = new blocco(altezza, larghezza);
-
+    //prende il controllo delle label di debug
     html_altez = document.getElementById("html_lh")
-    html_lungh = document.getElementById("html_wh")    
-}
+    html_lungh = document.getElementById("html_wh")  
+    html_lungh_snake = document.getElementById("lungh")
 
+    
+
+}
+//funzione che gestisce gli eventi keypress 
 function key_handler(event){
     console.log("pressed: key: " + event.key+ "")
     if(event.key == "a"){
@@ -61,14 +82,22 @@ function key_handler(event){
     }
     console.log("direzione attuale: " + direzione)
 }
-
+//funzione che aggiunge un blocco al serpente
+function add(){
+    array_x.push(array_x[-1]-60);
+    array_y.push(array_y)
+}
+//funzione che muove tutti i blocchi presenti nel serpente
 function move(){   
-    
+    for(i=array_x.length-1; i>0; i--){    
+        array_x[i] = array_x[i-1]
+        array_y[i] = array_y[i-1]
+    }
     switch (direzione) {
         case 0:
-                if(array_y[0]-ogg.height >= 0){
-                    array_y[0] -= ogg.height;
-                }
+            if(array_y[0]-ogg.height >= 0){
+                array_y[0] -= ogg.height;
+            }
             break            
         case 1:
             if(array_x[0]+ogg.width <= canv.width-ogg.width){
@@ -76,12 +105,12 @@ function move(){
             }          
             break
         case 2:
-            if(array_y[0]+ogg.height <= canv.height-ogg.width){
+            if(array_y[0]+ogg.height <= canv.height-ogg.width){            
                 array_y[0] += ogg.height;
             }    
             break
         case 3:
-            if(array_x[0]-ogg.width >= 0){
+            if(array_x[0]-ogg.width >= 0){              
                 array_x[0] -= ogg.height;
             }               
             break;
@@ -89,26 +118,41 @@ function move(){
             console.log("errore mistico nel movimento")
             break;
     }
-
+    
 
 }
-//funzione per 
+//funzione per pulire il canvas
 function clear_rect(){
     cont.clearRect(0, 0, canv.width, canv.height);
 }
 //funzione che disegna il blocco 
 function draw(){
     clear_rect();
-    cont.beginPath();    
+    
+    cont.beginPath();
 
-    cont.rect(array_x[0],array_y[0], ogg.width,ogg.width)
-  
+    move()
+
+    
+    for(x = 0; x < array_x.length; x++){   
+        drawBorder(array_x[x], array_y[x], ogg.width, ogg.height);
+        cont.fillStyle = "#21F94C"
+    
+        cont.fillRect(array_x[x],array_y[x], ogg.width,ogg.width)
+    }
+    
     cont.stroke();
     
-    move()
+    
     html_altez.innerHTML = array_y[0]
     html_lungh.innerHTML = array_x[0]
     console.log("x:" + array_x[0]+ " y:" + array_y[0]);
 }
-setInterval(main_handler, 80);
+//funzione per disegnare bordi intorno ai blocchi
+function drawBorder(xPos, yPos, width, height, thickness = 3)
+{
+  cont.fillStyle='#000   ';
+  cont.fillRect(xPos - (thickness), yPos - (thickness), width + (thickness * 2), height + (thickness * 2));
+}
 
+setInterval(main_handler, 80);
